f7879b151ef1281bfb4a58e20fd14033978890e6
Camuka <149012726+Camuka20284@users.noreply.github.com>
2024-02-15 00:58:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# Import / İçe Aktarma
-from flask import Flask, render_template
+# İçe Aktarma
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -15,9 +15,8 @@ def result_calculate(size, lights, device):
 @app.route('/')
 def index():
     return render_template('index.html')
-
 # İkinci sayfa
-@app.route('/<size>') # /1   /2    /3
+@app.route('/<size>')
 def lights(size):
     return render_template(
                             'lights.html', 
@@ -28,7 +27,7 @@ def lights(size):
 @app.route('/<size>/<lights>')
 def electronics(size, lights):
     return render_template(
-                            'electronics.html',
+                            'electronics.html',                           
                             size = size, 
                             lights = lights                           
                            )
@@ -42,4 +41,31 @@ def end(size, lights, device):
                                                     int(device)
                                                     )
                         )
-app.run(debug=True, port=5001)
+# Form
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+#Formun sonuçları
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    # Veri toplama için değişkenleri tanımlayın
+    name = request.form['isim']
+    adres= request.form['address']
+    date= request.form['date']
+
+    # Verilerinizi kaydedebilir veya e-posta ile gönderebilirsiniz
+    with open('form.txt', 'a',) as f:
+        f.write(name + '/'),
+        f.write(adres + '/'),
+        f.write(date + '/')
+
+    return render_template('form_result.html', 
+                           # Değişkenleri buraya yerleştirin
+                           name=name,
+                           adres=adres,
+                           date=date
+                           )
+
+
+app.run(debug=True)
